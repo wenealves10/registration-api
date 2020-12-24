a5b50a66ee60f954fa449b934b1576d4acf28e85
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-24 08:54:51 -0300

    reading data

--- controllers/HomeControllers.js
+++ controllers/HomeControllers.js
@@ -1,3 +1,13 @@
+const Read = require('../model/Read');
+
 module.exports = (req, res) =>{
-    res.send('Hello World');
+
+    Read.Reading('data.json').then(dat =>{
+        res.statusCode = 200;
+        res.json(Read.ReadParse(dat));
+    }).catch(err =>{
+        res.statusCode = 404
+        res.json({err: 'Erro Reading the file!!!'});
+    })
+    
 }
