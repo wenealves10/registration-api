4a3d341211a2953ca62dd8b3324acb9529bfcac5
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-24 09:31:55 -0300

    data creation route

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = express.Router();
 const HomeControllers = require('./controllers/HomeControllers');
-
+const CreateControllers = require('./controllers/CreateControllers');
 
 route.get('/',HomeControllers);
+route.post('/create',CreateControllers);
 
 module.exports = route;
