5f7fadc8712bd70ad07e146bf52a5f883f86742d
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-25 19:45:43 -0300

    improving the route

--- controllers/CreateControllers.js
+++ controllers/CreateControllers.js
@@ -1,22 +1,3 @@
-const Write = require('../model/Write');
-const Read = require('../model/Read');
-
 module.exports = (req, res) => {
-    const name = req.body.name;
-    const year = req.body.year;
-    Read.Reading('data.json').then(data =>{
-        const datas = Read.ReadParse(data);
-        datas.push({name, year});
-        const people = new Write('data.json',datas);
-        people.Writing().then(() =>{
-            res.statusCode = 200;
-            res.json({status:'Salve'});
-        }).catch(err =>{
-            res.statusCode = 400;
-            res.json({err: 'Erro salve'});
-        })
-    }).catch(err => {
-        res.statusCode = 404;
-        res.json({err:'Erro crete'});
-    })
+  res.json({save: 'Saved successfully'});
 }
