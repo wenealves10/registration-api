277e0f27e421fea6dca093a383bc0d803fbf27ae
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-26 09:14:37 -0300

    user control

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,3 +1,13 @@
+const Users = require('../model/Users');
+const users = new Users();
 module.exports = (req, res) => {
-    res.json({user: [{}]});
+  
+  if(users.getUser(req.params.id)){
+    res.statusCode = 200;
+    res.json({data:users.getUser(req.params.id)});
+  }else{
+    res.statusCode = 404;
+    res.json({error:'Not Found 404'});
   }
+
+}
