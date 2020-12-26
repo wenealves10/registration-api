9cd64a6513bf0b1e25fc897aae904f4915380cab
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-26 11:07:02 -0300

    route of login

---
+++ controllers/LoginControllers.js
@@ -0,0 +1,17 @@
+const Login = require('../model/Login');
+const userLogin = new Login();
+
+module.exports = (req, res) => {
+    userLogin.loginUser(req.body).then(token => {
+        if(token){
+            res.statusCode = 200;
+            res.json({token});
+        }else{
+            res.statusCode = 404;
+            res.json({error: 'wrong email or password'});
+        }
+    }).catch(error =>{
+        res.statusCode = 401;
+        res.json({error: 'wrong email or password'})
+    })
+}
