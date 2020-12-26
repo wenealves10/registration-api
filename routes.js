88d7b68d004410b5eb268c2843864cc5ac018b5a
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-26 11:07:37 -0300

    more routes

--- routes.js
+++ routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const route = express.Router();
+const auth = require('./middlewares/Auth');
 
 const UserControllers = require('./controllers/UserControllers');
 const UsersControllers = require('./controllers/UsersControllers');
 const CreateControllers = require('./controllers/CreateControllers');
 const DeleteControllers = require('./controllers/DeleteControllers');
 const UpdateControllers = require('./controllers/UpdateControllers');
+const LoginCreateControllers = require('./controllers/LoginCreateControllers');
+const LoginControllers = require('./controllers/LoginControllers');
 
-route.get('/users',UsersControllers);
-route.get('/user/:id',UserControllers);
-route.put('/user/:id',UpdateControllers);
-route.delete('/user/:id',DeleteControllers);
-route.post('/user/create',CreateControllers);
+route.get('/users',auth,UsersControllers);
+route.get('/user/:id',auth,UserControllers);
+route.put('/user/:id',auth,UpdateControllers);
+route.delete('/user/:id',auth,DeleteControllers);
+route.post('/user/create',auth,CreateControllers);
+route.post('/user/sign',auth,LoginCreateControllers);
+route.post('/user/login',LoginControllers);
 
 module.exports = route;
