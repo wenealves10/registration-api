f748b5684bdc56f02b57a88883f8e6dc75ea105b
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-26 09:11:22 -0300

    creation route

--- controllers/CreateControllers.js
+++ controllers/CreateControllers.js
@@ -1,3 +1,14 @@
+const Users = require('../model/Users');
+const users = new Users();
 module.exports = (req, res) => {
-  res.json({save: 'Saved successfully'});
+  // res.json({save: 'Saved successfully'});
+  const created = users.createUser(req.body);
+  if(created){
+    res.statusCode = 200;
+    res.json({status: 'Created successfully'});
+  }else{
+    res.statusCode = 401;
+    res.json({error:'Creation error'});
+  }
+  
 }
