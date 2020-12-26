99a038aee237d740ec9aa665761c55bd2632446f
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-26 09:10:55 -0300

    user route

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -1,3 +1,13 @@
+const Users = require('../model/Users');
+const users = new Users();
 module.exports = (req, res) => {
-    res.json({users:[{}]})
+    
+   try{
+       res.statusCode = 200;
+       res.json({data:users.getsUsers});
+   }catch(err){
+       res.statusCode = 401;
+       res.json({err:err});
+   }
+
 }
