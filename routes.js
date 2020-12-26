a6369f6d24d17faf6c63f936044677c8c59da30b
Wene Alves de Oliveira <weneplay5@gmail.com>
2020-12-26 09:12:27 -0300

    all routes

--- routes.js
+++ routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const route = express.Router();
+
+const UserControllers = require('./controllers/UserControllers');
 const UsersControllers = require('./controllers/UsersControllers');
 const CreateControllers = require('./controllers/CreateControllers');
-const UserControllers = require('./controllers/UserControllers');
+const DeleteControllers = require('./controllers/DeleteControllers');
+const UpdateControllers = require('./controllers/UpdateControllers');
 
 route.get('/users',UsersControllers);
 route.get('/user/:id',UserControllers);
-route.post('/create',CreateControllers);
+route.put('/user/:id',UpdateControllers);
+route.delete('/user/:id',DeleteControllers);
+route.post('/user/create',CreateControllers);
 
 module.exports = route;
